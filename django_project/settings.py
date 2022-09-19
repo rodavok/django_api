3e97ea2735ba8632d94bdc740ff5987817dd8174
rodavok <rodavok@DESKTOP-134UEK0.localdomain>
2022-09-19 18:11:26 -0400

    staticfiles issues...

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
-STATIC_ROOT = [BASE_DIR / "staticfiles"]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
